22203be5b8c842c07e547c12cf44933e0e8150a6
Jeff Deng <dengh@oregonstate.edu>
2021-11-10 18:03:15 -0800

    initialized xcode project with swiftui and swift5

---
+++ Foozy/Foozy/FoozyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoozyApp.swift
+//  Foozy
+//
+//  Created by Jeff Deng on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct FoozyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
