ef2d050c96c295d016644b9c3276d8b4df713176
Jeff Deng <dengh@oregonstate.edu>
2023-01-02 18:01:03 -0800

    able to display the view

--- Foozy/Foozy/ContentView.swift
+++ Foozy/Foozy/ContentView.swift
@@ -8,9 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+
     var body: some View {
         VStack{
-            
             // Top Stack
             HStack{
                 Button(action: {}) {
@@ -31,21 +32,73 @@ struct ContentView: View {
             }.padding(.horizontal)
             
             // Card
-            Image("sakura-noodle-house")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
+            CardView()
+                .padding(8)
             
             // Bottom Stack
-            HStack{
-                
+            HStack(spacing: 30){
+                Button(action: {}) {
+                    Image("previous-button")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: 45)
+                }
+                Button(action: {}) {
+                    Image("star")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: 45)
+                }
+                Button(action: {}) {
+                    Image("next-button")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: 45)
+                }
             }
         }
-//        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CardView: View {
+    // MARK: - Drawing Constants
+    let cardGradient = Gradient(colors: [Color.black.opacity(0), Color.black.opacity(0.5)])
+    var body: some View {
+        ZStack(alignment: .leading) {
+            Image("sakura-noodle-house")
+                .resizable()
+            LinearGradient(gradient: cardGradient, startPoint: .top, endPoint: .bottom)
+            
+            VStack{
+                Spacer()
+                VStack(alignment: .leading){
+                    
+                    // star rating
+                    StarsView(rating: 4.5, maxRating: 5)
+                        .frame(width: 100)
+                        .padding(.bottom, -10)
+                    
+                    // business name
+                    Text("Sakura Noodle House")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .minimumScaleFactor(0.01)
+                        .lineLimit(1)
+                    
+                    // categories
+                    Text("Ramen, Noodle, Food Truck")
+                }
+            }
+            .padding()
+            .foregroundColor(.white)
+            
+        }
+        .cornerRadius(10)
     }
 }
---
+++ Foozy/Foozy/Helpers/StarsView.swift
@@ -0,0 +1,37 @@
+//
+//  StarRating.swift
+//  Foozy
+//
+//  Created by Jeff Deng on 1/2/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct StarsView: View {
+    var rating: CGFloat
+    var maxRating: Int
+
+    var body: some View {
+        let stars = HStack(spacing: 0) {
+            ForEach(0..<maxRating, id: \.self) { _ in
+                Image(systemName: "star.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+        }
+
+        stars.overlay(
+            GeometryReader { g in
+                let width = rating / CGFloat(maxRating) * g.size.width
+                ZStack(alignment: .leading) {
+                    Rectangle()
+                        .frame(width: width)
+                        .foregroundColor(.red)
+                }
+            }
+            .mask(stars)
+        )
+        .foregroundColor(.gray)
+    }
+}
